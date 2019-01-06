2eacd5c350255da5f7f0ccdc879b376f1ad0ad7a
Robin Joshua <robin.joshua@promonsoft.com>
2019-01-06 23:52:48 +0530

    added DISQUS comment system

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -37,102 +37,27 @@
 
         <!-- Blog Comments -->
 
-            @if(Auth::check())
+        <div id="disqus_thread"></div>
+        <script>
 
-                <!-- Comments Form -->
-                <div class="well">
-                    <h4>Leave a Comment:</h4>
-
-                        {!! Form::open(['method'=>'POST','action'=>'PostCommentsController@store']) !!}
-
-                                <input type="hidden" name="post_id" value="{{$post->id}}" >
-                                <div class="form-group">
-
-                                    {!! Form::label('body','Comment') !!}
-                                    {!! Form::textarea('body',null,['class'=>'form-control','rows'=>3]) !!}
-
-                                </div>
-                                <div class="form-group">
-
-                                    {!! Form::submit('Submit Comment',['class'=>'btn btn-primary']) !!}
-
-                                </div>
-
-
-
-                        {!! Form::close() !!}
-                </div>
-            @endif
-
-        <hr>
-
-        <!-- Posted Comments -->
-
-
-        <!-- Comment -->
-
-        @if(count($comments) > 0)
-            @foreach($comments as $comment)
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img height="64"class="media-object" src="{{$comment->photo}}" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">{{$comment->author}}
-                            <small>{{$comment->created_at->diffForHumans()}}</small>
-                        </h4>
-                        <p>{{$comment->body}}</p>
-
-
-
-                        @foreach($comment->replies as $reply)
-
-                            @if($reply->is_active==1)
-
-                                <!-- Nested Comment -->
-                                <div id="nested-comment" class="media">
-                                    <a class="pull-left" href="#">
-                                        <img height="54" class="media-object" src="{{$reply->photo}}" alt="">
-                                    </a>
-                                    <div class="media-body">
-                                        <h4 class="media-heading">{{$reply->author}}
-                                            <small>{{$reply->created_at->diffForHumans()}}</small>
-                                        </h4>
-                                        <p>{{$reply->body}}</p>
-                                    </div>
-                                </div>
-                                <!-- End Nested Comment -->
-                            @endif
-                        @endforeach
-
-                        <div class="comment-reply-container">
-
-                            <button class="toggle-reply btn btn-primary pull-right">Reply</button>
-
-                            <div class="comment-reply col-sm-9">
-
-                                {!! Form::open(['method'=>'POST','action'=>'CommentRepliesController@createReply']) !!}
-
-                                        <input type="hidden" name="comment_id" value="{{$comment->id}}" >
-                                        <div class="form-group">
-
-                                            {!! Form::label('body','Body:') !!}
-                                            {!! Form::text('body',null,['class'=>'form-control','rows'=>1]) !!}
-
-                                        </div>
-                                        <div class="form-group">
-
-                                            {!! Form::submit('Submit Reply',['class'=>'btn btn-primary']) !!}
-
-                                        </div>
-
-                                {!! Form::close() !!}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        @endif
+            /**
+             *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+             *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+            /*
+            var disqus_config = function () {
+            this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+            this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+            };
+            */
+            (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://codehacking-test-3.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        <script id="dsq-count-scr" src="//codehacking-test-3.disqus.com/count.js" async></script>
 
 
 @endsection
