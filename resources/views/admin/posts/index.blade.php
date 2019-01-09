cd750245134d5918334f9b35899465508b096317
Robin Joshua <robin.joshua@promonsoft.com>
2019-01-09 23:37:36 +0530

    Making the front end more modular

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -16,10 +16,9 @@
          <tr>
              <th>Id</th>
              <th>Photo</th>
+             <th>Title</th>
              <th>Owner</th>
              <th>Category</th>
-             <th>Title</th>
-             <th>Body</th>
              <th>Created</th>
              <th>Updated</th>
              <th>Post Link</th>
@@ -32,10 +31,9 @@
                 <tr>
                     <td>{{$post->id}}</td>
                     <td><img height="50" src="{{$post->photo ? $post->photo->file: '/images/400.png'}}" alt=""></td>
+                    <td> <a href="{{route('admin.posts.edit',$post->id)}}">{{$post->title}}</a></td>
                     <td>{{$post->user->name}}</td>
                     <td>{{$post->category? $post->category->name : 'Uncategorized'}}</td>
-                    <td> <a href="{{route('admin.posts.edit',$post->id)}}">{{$post->title}}</a></td>
-                    <td>{!! str_limit($post->body,30) !!}</td>
                     <td>{{$post->created_at->diffForHumans()}}</td>
                     <td>{{$post->updated_at->diffForHumans()}}</td>
                     <td><a href="{{route('home.post',$post->slug)}}">View Post</a></td>
