f4798667f3d14f9b131e986252f7357cf955c47d
Robin Joshua <robin.joshua@promonsoft.com>
2019-01-11 00:29:29 +0530

    changed link to route for home button

--- resources/views/includes/front_nav.blade.php
+++ resources/views/includes/front_nav.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">Home</a>
+            <a class="navbar-brand" href="{{route('home')}}">Home</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ if(version_compare(PHP_VERSION,'7.2.0','>=')){
     error_reporting(E_ALL ^ E_NOTICE ^E_WARNING);
 }
 
-Route::get('/', 'HomeController@index');
+Route::get('/', ['as'=>'home','uses'=>'HomeController@index']);
 
 Auth::routes();
 
