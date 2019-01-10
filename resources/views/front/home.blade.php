62aecc952d404176699d0041f12d313264551fa7
Robin Joshua <robin.joshua@promonsoft.com>
2019-01-10 21:55:28 +0530

    refactoring home page

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,17 +1,39 @@
 @extends('layouts.blog-home')
 
 @section('content')
-<div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        @if($posts)
 
-                <div class="panel-body">
-                    You are logged in!
+            @foreach($posts as $post)
+                <!-- Blog Entries Column -->
+                <div class="col-md-8">
+
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="#">{{$post->title}}</a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php">{{$post->user->name}}</a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> {{$post->created_at->diffForHumans()}}</p>
+                    <hr>
+                    <img class="img-responsive" src="{{$post->photo ? $post->photo->file :'http://placehold.it/900x300'}}" alt="">
+                    <hr>
+                    <p>{{str_limit($post->body,300)}}</p>
+                    <a class="btn btn-primary" href="{{route('home.post',$post->slug)}}">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
+
+                    <!-- Pagination -->
+
                 </div>
-            </div>
-        </div>
+            @endforeach
+
+            {{$posts->render()}}
+        @endif
+        <!-- Blog Sidebar Widgets Column -->
+        @include('includes.front_sidebar')
+
     </div>
-</div>
+    <hr>
 @endsection
